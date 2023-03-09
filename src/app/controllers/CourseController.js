6b2bd2d3773cc491b9b091eb1f723a3225c8321b
xuanTinhhh <117650187+xuantinhfedev@users.noreply.github.com>
2023-03-09 13:38:13 +0700

    delete courses

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -25,23 +25,31 @@ class SiteController {
         const course = new Course(formData);
         course.save().then(() => {
             res.redirect('/')
-        }).catch(error => {});
+        }).catch(error => { });
     }
 
     // [GET] /courses/:id/edit
     edit(req, res, next) {
 
         Course.findById(req.params.id)
-        .then(course =>  res.render('courses/edit', {
-            course: mongooseToObject(course),
-        }))
-        .catch(next)
+            .then(course => res.render('courses/edit', {
+                course: mongooseToObject(course),
+            }))
+            .catch(next)
     }
     // [PUT] /courses/:id
     update(req, res, next) {
-        Course.updateOne({_id: req.params.id}, req.body)
-        .then(() => res.redirect('/me/stored/courses'))
-        .catch(next)
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect('/me/stored/courses'))
+            .catch(next)
+    }
+
+    // [DELETE] /courses/:id
+    destroy(req, res, next) {
+
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
     }
 }
 
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,12 +2,28 @@
 const mongoose = require('mongoose');
 
 async function connect() {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/blog_education_dev');
-        console.log("Connect successfully@@!!!")
-    } catch (error) {
-        console.log("Connect failure@@!!!")
-    }      
+    // try {
+    //     await mongoose.connect('mongodb://127.0.0.1:27017/blog_education_dev', {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //         useCreateIndex: true
+    //     });
+    //     console.log("Connect successfully@@!!!")
+    // } catch (error) {
+    //     console.log("Connect failure@@!!!")
+    // }
+
+    mongoose
+        .connect(`mongodb://127.0.0.1:27017/blog_education_dev`)
+        .then(() => {
+            useNewUrlParser: true
+            useUnifiedTopology: true
+            useCreateIndex: true
+            console.log('MongoDB connected!!')
+        })
+        .catch(err => {
+            console.log('Failed to connect to MongoDB', err)
+        })
 }
 
 module.exports = { connect }
